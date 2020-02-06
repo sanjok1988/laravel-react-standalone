b312d1b44fd8530d7514922cf674c73111c14cf9
sanjok-cdts <sanjok.dangol@cdts.com.np>
2020-02-06 15:59:27 +0545

    redux added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import {hot} from 'react-hot-loader';
 import './styles/App.css';
+import { Provider } from 'react-redux';
+import store from './stores/index.js';
+import UserForm from './components/Users/UserForm.js';
 
 function App() {
 
   return (
     <div>
       <h1>Hello</h1>
-      <p>What a REAVELation</p>
+      <p>Wow Excellent</p>
+      <Provider store={store}>
+        <UserForm/>
+      </Provider>
     </div>
   );
 }
